32cea05ed1941e5c22e3dea047f650a7fa78ee94
macacolabs <monkey.ohgiraffers@gmail.com>
2025-05-14 16:30:00 +0900

    7th period upload

--- 00_YUSEUNGJE/module08-redux-store/src/app/chap03/page.jsx
+++ 00_YUSEUNGJE/module08-redux-store/src/app/chap03/page.jsx
@@ -1,6 +1,11 @@
 "use client";
 
+import {
+  decrement,
+  increment,
+} from "@/components/chap03/slice/CreateAsyncThunk";
 import { useSelector, useDispatch } from "react-redux";
+import { fetchUser } from "@/components/chap03/thunk/ThunkReducer";
 
 function Section01() {
   const state = useSelector((state) => state.counter);
@@ -13,9 +18,13 @@ function Section01() {
       <h1>CreateSlice</h1>
       <div>
         <p>Count : {state.value}</p>
+        <button onClick={() => dispatch(increment())}>+</button>
+        <button onClick={() => dispatch(decrement())}>-</button>
       </div>
       <div>
         <p>Status : {userState.state}</p>
+        <p>Data : {JSON.stringify(userState.data)}</p>
+        <button onClick={() => dispatch(fetchUser())}>Fetch User</button>
       </div>
     </>
   );
--- 00_YUSEUNGJE/module08-redux-store/src/components/chap03/store.jsx
+++ 00_YUSEUNGJE/module08-redux-store/src/components/chap03/store.jsx
@@ -1,10 +1,12 @@
 // 저장소에 어떤 Reducer가 들어가있는지 확인가능
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "./slice/CreateAsyncThunk";
+import userReducer from "./thunk/ThunkReducer";
 
 const store = configureStore({
   reducer: {
     counter: counterReducer,
+    user: userReducer,
   },
 });
 
